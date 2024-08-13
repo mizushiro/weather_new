884003d4ca70b552745dd1e3cdf2663c51bc6c19
es-kangjy <mizushiro@naver.com>
2024-08-13 10:54:06 +0900

    sub ing

--- resources/js/ui.common.js
+++ resources/js/ui.common.js
@@ -1,50 +1,38 @@
- document.addEventListener('DOMContentLoaded', () => {
-    UI.parts.include({
-        src: 'header.html',
-        dataId: 'header',
-        callback:() => {
-            UI.exe.toggle = new ToggleUI();
-
-            UI.exe.accordion = new Accordion({
-                id :'navAcco',
-                current: 0,
-                callback: (v) => {
-                    console.log('callback:', v);
-                }
-            });
-        }
-    });
-    UI.parts.include({
-        src: 'footer.html',
-        dataId: 'footer',
-        callback:() => {
-            const el_top = document.querySelector('.floating-top');
-
-            el_top.addEventListener('click', () => {
-                UI.scroll.move({
-                    top: 0
-                })
-            });
-
-            UI.exe.toggle = new ToggleUI();
-            UI.exe.modal_sass_0213 = new Layer({
-                id :'modal_sass_0213',
-                type: 'modal',
-                src: 'SASS_0213_modal.html',
-                callback: () => {
-                }
-            });
-        }
-    });
-
-    UI.callback.nav = (v) => {
-        console.log(v.state);
-        const wrap = document.querySelector('html');
-
-        if (v.state === 'true') {
-            wrap.dataset.nav = 'open';
-        } else {
-            wrap.dataset.nav = 'close';
-        }
-    }
-});
+ var newWeather = (function() {
+         var callLayer = function() {
+             var btnLayer = document.querySelectorAll('[data-layer]');
+             for (var i = 0; i < btnLayer.length; i++) {
+                 btnLayer[i].addEventListener('click', function(e) {
+                     var targetId = this.dataset.layer;
+                     var targetLayer = document.querySelector('#' + targetId);
+                     targetLayer.classList.add('showing');
+                     console.log($(targetLayer));
+                     if ($(targetLayer).is('.layer-area')) {
+                         $('html, body').addClass('scroll-off');
+                     }
+                 });
+             }
+             var btnLayerClose = document.querySelectorAll('.layer_section .btn_layer-close');
+             for (var i = 0; i < btnLayerClose.length; i++) {
+                 btnLayerClose[i].addEventListener('click', function(e) {
+                     var targetElem = e.target;
+                     while (!targetElem.classList.contains('layer_section')) {
+                         targetElem = targetElem.parentNode;
+                         if (targetElem.nodeName == 'BODY') {
+                             targetElem = null;
+                             return;
+                         }
+                     }
+                     targetElem.classList.remove('showing');
+                 })
+             }
+         };
+         return {
+             callLayer: callLayer,
+         }
+     }
+ )();
+ document.addEventListener('DOMContentLoaded', function () {
+     //layerpopup
+     newWeather.callLayer();
+ }, false);
